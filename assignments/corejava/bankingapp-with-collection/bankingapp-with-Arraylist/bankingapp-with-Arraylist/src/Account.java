14dcd0af7f86114b978235a8d56336da05a7f84c
stella <stelladavies29@gmail.com>
2023-09-21 13:36:18 +0530

    BankingAppusingArraylist

---
+++ assignments/corejava/bankingapp-with-collection/bankingapp-with-Arraylist/bankingapp-with-Arraylist/src/Account.java
@@ -0,0 +1,47 @@
+
+
+public class Account {
+	
+	int id;
+	String name;
+	String type;
+	Double balance;
+	boolean active;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", type=" + type + ", balance=" + balance + ", active=" + active
+				+ "]";
+	}
+	
+
+}
