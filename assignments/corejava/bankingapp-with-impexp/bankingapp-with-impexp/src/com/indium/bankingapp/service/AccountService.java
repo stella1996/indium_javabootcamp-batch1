b80c37abb0a41d70ed2c205e47417a5bef296d73
stella <stelladavies29@gmail.com>
2023-09-28 07:02:26 +0530

    BankingAppwithImportandExport

---
+++ assignments/corejava/bankingapp-with-impexp/bankingapp-with-impexp/src/com/indium/bankingapp/service/AccountService.java
@@ -0,0 +1,35 @@
+package com.indium.bankingapp.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import com.indium.bankingapp.exception.AccountException;
+import com.indium.bankingapp.model.Account;
+
+public interface AccountService {
+
+	public boolean createAccount(Account acc);
+
+	public boolean updateAccount(Account acc);
+
+	public boolean deleteAccount(int accountId) throws AccountException;
+
+	public Account getAccount(int accountId) throws AccountException;
+
+	public Collection getAll();
+
+	public int getAccountsCountBalanceMorethanOneLakh(int i);
+
+	public Map<String, Long> getAccountsCountByAccountType();
+
+	public Map<String, Long> getAccountsCountByAccountTypeOrdered();
+
+	public Map<String, Double> getAverageBalanceByAccountType();
+
+	public Collection getAccountDetailsByGivenName();
+
+	public void bulkExport();
+
+	public void bulkImport();
+
+}
