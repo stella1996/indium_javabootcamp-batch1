995412f0debd0ea83799e3b356669a69231fabfe
stella <stelladavies29@gmail.com>
2023-09-28 12:30:02 +0530

    BankingAppUsingJDBC

---
+++ assignments/corejava/bankingapp-with-jdbc/bankingapp-with-jdbc/src/com/indium/bankingapp/service/AccountService.java
@@ -0,0 +1,34 @@
+package com.indium.bankingapp.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import com.indium.bankingapp.model.Account;
+
+public interface AccountService {
+
+	public boolean createAccount(Account acc);
+
+	public boolean updateAccount(Account acc);
+
+	public boolean deleteAccount(int accountId);
+
+	public Account getAccount(int accountId);
+
+	public Collection getAll();
+
+	public long getAccountsCountBalanceMorethanOneLakh(int i);
+
+	public Map<String, Long> getAccountsCountByAccountType();
+
+	public Map<String, Long> getAccountsCountByAccountTypeOrdered();
+
+	public Map<String, Double> getAverageBalanceByAccountType();
+
+	public Collection getAccountDetailsByGivenName();
+
+	public void bulkExport();
+
+	public void bulkImport();
+
+}
